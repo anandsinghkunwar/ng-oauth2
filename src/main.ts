ee0672ff6beaa9cce693c577b655825595b12329
Anand Singh Kunwar <anandkunwar95@gmail.com>
2017-04-04 21:57:11 +0530

    Refactoring the name of the actual App

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 /// <reference path='_all.ts' />
 
 /**
- * The main TodoMVC app module.
+ * The main ng-oauth2 app module.
  *
  * @type {angular.Module}
  */
 module todos {
     'use strict';
 
-    var ngOAuth2 = angular.module('todomvc', [])
+    var ngOAuth2 = angular.module('ngOAuth2', [])
             .service('todoStorage', TodoStorage);
 }
